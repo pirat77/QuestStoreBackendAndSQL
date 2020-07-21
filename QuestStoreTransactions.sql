f6960003dc32f1b26710c3dc25861dba3d8bd5d5
pirat77 <58707909+pirat77@users.noreply.github.com>
2020-07-21 22:15:17 +0200

    Add files via upload
    Adds sample transactions into database

---
+++ QuestStoreTransactions.sql
@@ -0,0 +1,29 @@
+INSERT INTO students_details_quests (student_id, quest_id, date)
+VALUES (1, 2, to_date('12 Dec 2012', 'DD Mon YYYY'));
+INSERT INTO students_details_quests (student_id, quest_id, date)
+VALUES (2, 2, to_date('12 Dec 2012', 'DD Mon YYYY'));
+INSERT INTO students_details_quests (student_id, quest_id, date)
+VALUES (3, 1, to_date('17 Nov 2017', 'DD Mon YYYY'));
+INSERT INTO students_details_quests (student_id, quest_id, date)
+VALUES (3, 3, to_date('13 Sep 2008', 'DD Mon YYYY'));
+INSERT INTO students_details_quests (student_id, quest_id, date)
+VALUES (4, 4, to_date('11 Dec 2019', 'DD Mon YYYY'));
+INSERT INTO students_details_quests (student_id, quest_id, date)
+VALUES (5, 4, to_date('11 Nov 2011', 'DD Mon YYYY'));
+INSERT INTO students_details_quests (student_id, quest_id, date)
+VALUES (6, 6, to_date('18 Dec 2020', 'DD Mon YYYY'));
+
+INSERT INTO students_details_artifacts (student_id, artifact_id, date)
+VALUES (3, 3, to_date('12 Nov 2007', 'DD Mon YYYY'));
+INSERT INTO students_details_artifacts (student_id, artifact_id, date)
+VALUES (1, 2, to_date('15 Oct 2010', 'DD Mon YYYY'));
+INSERT INTO students_details_artifacts (student_id, artifact_id, date)
+VALUES (4, 8, to_date('13 Dec 2013', 'DD Mon YYYY'));
+INSERT INTO students_details_artifacts (student_id, artifact_id, date)
+VALUES (5, 9, to_date('12 Dec 2012', 'DD Mon YYYY'));
+INSERT INTO students_details_artifacts (student_id, artifact_id, date)
+VALUES (5, 4, to_date('13 Dec 2042', 'DD Mon YYYY'));
+INSERT INTO students_details_artifacts (student_id, artifact_id, date)
+VALUES (7, 2, to_date('11 Dec 2032', 'DD Mon YYYY'));
+INSERT INTO students_details_artifacts (student_id, artifact_id, date)
+VALUES (10, 8, to_date('10 Dec 2022', 'DD Mon YYYY'));
