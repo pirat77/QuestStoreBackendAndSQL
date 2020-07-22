0e84821427e895533070d446ec6c92884c3eee10
Jasio <mcyanek@gmail.com>
2020-07-22 14:20:58 +0200

    adds script which populates classrooms with users

---
+++ QuestStoreClassRooms.sql
@@ -0,0 +1,4 @@
+INSERT INTO classrooms (id, name) VALUES ('1', 'ProgBasic');
+INSERT INTO classrooms (id, name) VALUES ('2', 'OOP');
+INSERT INTO classrooms (id, name) VALUES ('3', 'Web');
+INSERT INTO classrooms (id, name) VALUES ('4', 'Advance');
---
+++ QuestStorePopulateClassrooms.sql
@@ -0,0 +1,33 @@
+ALTER TABLE users_classrooms 
+RENAME COLUMN mentor_id TO user_id;
+
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('1', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('2', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('3', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('4', '1');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('4', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('4', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('5', '1');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('5', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('5', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('6', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('6', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('6', '4');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('7', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('7', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('7', '4');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('8', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('9', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('10', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('11', '1');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('12', '1');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('13', '1');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('14', '4');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('15', '4');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('16', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('17', '2');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('18', '3');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('19', '4');
+INSERT INTO users_classrooms (user_id, classroom_id) VALUES ('20', '4');
+
+
