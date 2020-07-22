0e3634a23ac1693ce43fa507e71160e9191c882f
Dominik <domek99@gmail.com>
2020-07-22 11:06:04 +0200

    add second querry

--- AdminQuerries.sql
+++ AdminQuerries.sql
@@ -1,1 +1,15 @@
 --querries suitable for admin
+
+
+
+
+
+
+
+
+
+SELECT name, count(user_type_id) AS counter,
+       count(user_type_id) / (SELECT cast(count(user_type_id) AS FLOAT) FROM users) * 100 AS percentage
+FROM users JOIN user_types ut on users.user_type_id = ut.id
+GROUP BY name
+ORDER BY counter ASC;
