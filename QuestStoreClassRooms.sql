dbf29ec754e742cf3dee4604de25a09fb1e9d2ca
Jasio <mcyanek@gmail.com>
2020-07-23 09:32:25 +0200

    adds secret classroom into classrooms

--- QuestStoreClassRooms.sql
+++ QuestStoreClassRooms.sql
@@ -2,3 +2,4 @@ INSERT INTO classrooms (id, name) VALUES ('1', 'ProgBasic');
 INSERT INTO classrooms (id, name) VALUES ('2', 'OOP');
 INSERT INTO classrooms (id, name) VALUES ('3', 'Web');
 INSERT INTO classrooms (id, name) VALUES ('4', 'Advance');
+INSERT INTO classrooms (id, name) VALUES (5, 'Secret training');
